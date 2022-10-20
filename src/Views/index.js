ade55ce4e9e4c63832521d1b83e9e982be5376d6
Jason Goemaat <jason.goemaat@gmail.com>
2022-10-20 11:52:09 -0500

    Routing working

---
+++ src/Views/index.js
@@ -0,0 +1,4 @@
+import Home from './CorpSim.vue'
+import CorpSim from './CorpSim.vue'
+
+export { Home, CorpSim }
--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+// import { CorpSim, Home } from './Views'
+import CorpSim from './Views/CorpSim.vue'
+import Home from './Views/Home.vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+console.log('VIEWS: ', { CorpSim, Home })
+
+const routes = [
+  { path: '/', name: 'Home', component: Home },
+  { path: '/corp', name: 'CorpSim', component: CorpSim }
+]
+
+const history = createWebHistory()
+
+// createApp(App, { props: { routes: Object, name: String } }, { props: { routes, name: 'andy' } })
+createApp(App, { routes, name: 'andy' })
+.use(createRouter({ history, routes }))
+.mount('#app')
