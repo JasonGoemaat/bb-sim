ef9e8aacf31f798500dc5cc7a367346630b8488f
Jason Goemaat <jason.goemaat@gmail.com>
2022-10-20 20:54:47 -0500

    File input element at least getting file list

--- src/Views/CorpSim.vue
+++ src/Views/CorpSim.vue
@@ -1,4 +1,6 @@
 <script setup>
+import UploadFile from '../components/UploadFile.vue'
+
 </script>
 
 <template>
@@ -7,6 +9,7 @@
       <h1>CorpSim</h1>
     </div>
     <div class="content">
+      <UploadFile></UploadFile>
       <p>Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet.</p>
       <p>Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet.</p>
       <p>Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet.</p>
---
+++ src/components/UploadFile.vue
@@ -0,0 +1,16 @@
+<script setup>
+// https://developer.mozilla.org/en-US/docs/Web/API/File_API/Using_files_from_web_applications
+const handleFiles = (event) => {
+  console.log('handleFiles event:', event)
+  const fileList = event.target.files
+  console.log('handleFiles fileList:', fileList)
+}
+
+</script>
+
+<template>
+  Upload Files: <input ref="filesInputElement" @change="handleFiles" type="file" id="input" multiple />
+</template>
+
+<style scoped>
+</style>
